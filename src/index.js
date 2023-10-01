8f783c0ac535729094c3f2c2613f74b71f78facb
eeyfesubalew <esubalew.seife@gmail.com>
2023-10-01 17:15:56 +0300

    redux file structure

--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import "./store";
+import store from "./store";
+import { deposit } from "./features/accounts/accountSlice";
+
+store.dispatch({ type: "account/deposit", payload: 250 });
+store.dispatch(deposit(100));
+console.log(store.getState());
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
