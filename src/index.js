70b4117807a5617e3a247411f31fe382cac59fef
eeyfesubalew <esubalew.seife@gmail.com>
2023-10-01 17:52:20 +0300

    connecting react to redux

--- src/index.js
+++ src/index.js
@@ -3,14 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import store from "./store";
-import { deposit } from "./features/accounts/accountSlice";
+import { Provider } from "react-redux";
 
-store.dispatch({ type: "account/deposit", payload: 250 });
-store.dispatch(deposit(100));
-console.log(store.getState());
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
