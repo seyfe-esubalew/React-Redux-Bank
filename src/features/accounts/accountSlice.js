028266721cc41477290ca4860efa40bc5c41a811
seyfe-esubalew <esubalew.seife@gmail.com>
2023-10-02 08:52:50 +0300

    creating customer slice

--- src/features/accounts/accountSlice.js
+++ src/features/accounts/accountSlice.js
@@ -13,6 +13,7 @@ const accountSlice = createSlice({
   reducers: {
     deposit(state, action) {
       state.balance = state.balance + action.payload;
+      state.isLoading = false;
     },
     withdraw(state, action) {
       state.balance = state.balance - action.payload;
@@ -35,12 +36,27 @@ const accountSlice = createSlice({
       state.loan = 0;
       state.loanPurpose = "";
     },
+    convertingCurrency(state) {
+      state.isLoading = true;
+    },
   },
 });
 console.log(accountSlice.actions);
-export const { deposit, withdraw, requestLoan, payLoan } = accountSlice.actions;
-console.log(requestLoan(1000, "Buy a car"));
-console.log(payLoan(100));
+export const { withdraw, requestLoan, payLoan } = accountSlice.actions;
+export function deposit(amount, currency) {
+  if (currency === "USD") return { type: "account/deposit", payload: amount };
+  return async function (dispatch, getState) {
+    dispatch({ type: "account/convertingCurrency" });
+    //API call
+    const res = await fetch(
+      `https://api.frankfurter.app/latest?amount=${amount}&from=${currency}&to=USD`
+    );
+    const data = await res.json();
+    const converted = data.rates.USD;
+    //return action
+    dispatch({ type: "account/deposit", payload: converted });
+  };
+}
 export default accountSlice.reducer;
 
 /*
